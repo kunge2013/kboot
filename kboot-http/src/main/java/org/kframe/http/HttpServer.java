87f69ed9acf4bc9f8ce62d7c09023691be3cd36a
Test Git <710784360@qq.com>
2020-09-29 13:03:04 +0800

    添加 http server 信息

---
+++ kboot-http/src/main/java/org/kframe/http/HttpServer.java
@@ -0,0 +1,47 @@
+package org.kframe.http;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author fangkun
+ * @date 2020/9/29 11:40
+ * @description: httpserver 配置
+ */
+public class HttpServer extends Server {
+
+    static final boolean SSL = System.getProperty("ssl") != null;
+    static final int PORT = Integer.parseInt(System.getProperty("port", SSL? "8443" : "8080"));
+
+    int port ;
+
+    public HttpServer(int port){
+        this.port = port;
+    }
+
+    public void start() throws Exception{
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup work = new NioEventLoopGroup();
+        bootstrap.group(boss,work)
+                .handler(new LoggingHandler(LogLevel.DEBUG))
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new HttpHelloWorldServerInitializer(null));
+
+        ChannelFuture f = bootstrap.bind(new InetSocketAddress(port)).sync();
+        System.out.println(" server start up on port : " + port);
+        f.channel().closeFuture().sync();
+
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        new HttpServer(8888).start();
+    }
+}
---
+++ kboot-http/src/main/java/org/kframe/http/Server.java
@@ -0,0 +1,9 @@
+package org.kframe.http;
+
+/**
+ * @author fangkun
+ * @date 2020/9/29 11:40
+ * @description:
+ */
+public abstract class Server {
+}
