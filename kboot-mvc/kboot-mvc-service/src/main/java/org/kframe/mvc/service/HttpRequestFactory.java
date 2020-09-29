01a7d972847704ecabf24703376ee1386d923e0a
Test Git <710784360@qq.com>
2020-09-29 16:51:30 +0800

    添加http 先关注解信息

--- kboot-mvc/kboot-mvc-service/src/main/java/org/kframe/mvc/service/HttpRequestFactory.java
+++ kboot-mvc/kboot-mvc-service/src/main/java/org/kframe/mvc/service/HttpRequestFactory.java
@@ -3,17 +3,19 @@ package org.kframe.mvc.service;
 import org.kframe.mvc.api.Creator;
 import org.kframe.mvc.api.Request;
 
-import java.net.http.HttpRequest;
-
 /**
  * @author fangkun
  * @date 2020/9/29 15:33
  * @description:
  */
-public class HttpRequestFactory implements Creator<HttpRequest> {
+public  class HttpRequestFactory  {
 
-    @Override
-    public HttpRequest create(Object... objs) {
+    /**
+     * 创建 请求 创建器
+     * @return
+     */
+    protected  Creator<Request> createRequestCreator() {
         return null;
     }
+
 }
