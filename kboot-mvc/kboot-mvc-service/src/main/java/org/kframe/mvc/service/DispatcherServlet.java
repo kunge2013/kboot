ec3c484e1b4ef3772273c640a4c797f9c537336f
Test Git <710784360@qq.com>
2020-10-27 10:24:40 +0800

    添加http 先关注解信息

--- kboot-mvc/kboot-mvc-service/src/main/java/org/kframe/mvc/service/DispatcherServlet.java
+++ kboot-mvc/kboot-mvc-service/src/main/java/org/kframe/mvc/service/DispatcherServlet.java
@@ -1,6 +1,10 @@
 package org.kframe.mvc.service;
 
+import org.kframe.mvc.api.HandlerExecutionChain;
+import org.kframe.mvc.api.HandlerMapping;
+
 import java.net.http.HttpRequest;
+import java.util.List;
 
 /**
  * @author fangkun
@@ -9,8 +13,37 @@ import java.net.http.HttpRequest;
  */
 public class DispatcherServlet extends HttpServlet {
 
+    private List<HandlerMapping> handlerMappings;
+
     @Override
     public void doService(HttpRequest request) {
+        logRequest(request);
+        setReqAttribute(request);
+    }
+
+    /**
+     * 請求日志打印
+     * @param request
+     */
+    private void logRequest(HttpRequest request) {
 
     }
+
+    private void setReqAttribute(HttpRequest request) {
+
+    }
+
+    private void doDispatch(HttpRequest request) {
+
+    }
+
+    private HandlerExecutionChain parseHandler(HttpRequest request) {
+        for (HandlerMapping handlerMapping : handlerMappings) {
+            //handlerMapping.getHandler(request);
+        }
+        return null;
+    }
+
+
+
 }
