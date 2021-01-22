c497018922a80d206701e4455b323470fcad34b3
xuance <fk102191>
2021-01-22 10:12:24 +0800

    延迟队列线程池

--- kboot-common/src/main/java/com/kframe/common/executor/DelayThreadPoolExecutor.java
+++ kboot-common/src/main/java/com/kframe/common/executor/DelayThreadPoolExecutor.java
@@ -1,6 +1,10 @@
 package com.kframe.common.executor;
 
+import java.util.AbstractQueue;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author fangkun
@@ -10,6 +14,7 @@ import java.util.concurrent.*;
  * @description: 延迟队列线程池实现
  */
 public class DelayThreadPoolExecutor extends ThreadPoolExecutor implements ScheduledMessageExecutorService {
+
 
     public DelayThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
@@ -31,5 +36,86 @@ public class DelayThreadPoolExecutor extends ThreadPoolExecutor implements Sched
 
     }
 
+    static class ScheduledMsgFutureTask<V>  extends FutureTask<V> implements RunnableScheduledFuture<V> {
+
+        public ScheduledMsgFutureTask(Callable<V> callable) {
+            super(callable);
+        }
+
+        public ScheduledMsgFutureTask(Runnable runnable, V result) {
+            super(runnable, result);
+        }
+
+        public boolean isPeriodic() {
+            return false;
+        }
+
+        public long getDelay(TimeUnit unit) {
+            return 0;
+        }
+
+        public int compareTo(Delayed o) {
+            return 0;
+        }
+    }
+
+
+    static class DelayMessageQueue extends AbstractQueue<Runnable> implements BlockingQueue<Runnable>{
+
+        private final ReentrantLock lock = new ReentrantLock();
+
+        private static final int INITIAL_CAPACITY = 10;
+
+        private ScheduledMsgFutureTask<?>[] queue = new ScheduledMsgFutureTask<?>[INITIAL_CAPACITY];
+
+        public Iterator<Runnable> iterator() {
+            return null;
+        }
+
+        public int size() {
+            return 0;
+        }
+
+        public boolean offer(Runnable runnable) {
+            return false;
+        }
+
+        /*blkq */
+        public void put(Runnable runnable) throws InterruptedException {
+
+        }
+
+        public boolean offer(Runnable runnable, long timeout, TimeUnit unit) throws InterruptedException {
+            return false;
+        }
+
+        public Runnable take() throws InterruptedException {
+            return null;
+        }
+
+        public Runnable poll(long timeout, TimeUnit unit) throws InterruptedException {
+            return null;
+        }
+
+        public int remainingCapacity() {
+            return 0;
+        }
+
+        public int drainTo(Collection<? super Runnable> c) {
+            return 0;
+        }
+
+        public int drainTo(Collection<? super Runnable> c, int maxElements) {
+            return 0;
+        }
+
+        public Runnable poll() {
+            return null;
+        }
+
+        public Runnable peek() {
+            return null;
+        }
+    }
 
 }
