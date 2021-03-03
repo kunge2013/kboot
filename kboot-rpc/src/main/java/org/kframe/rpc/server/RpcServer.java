7070570bf9befe25985c55d3511bf43634a56735
xuance <fk102191>
2021-03-03 14:50:40 +0800

    rpc nio实现

--- kboot-rpc/src/main/java/org/kframe/rpc/server/RpcServer.java
+++ kboot-rpc/src/main/java/org/kframe/rpc/server/RpcServer.java
@@ -8,7 +8,7 @@ import java.io.IOException;
  * @date 2021/3/3 1:36 下午
  * @description:
  */
-public interface RpcServer extends Server{
+public interface RpcServer extends Server {
 
     void initService(String... packages);
 
